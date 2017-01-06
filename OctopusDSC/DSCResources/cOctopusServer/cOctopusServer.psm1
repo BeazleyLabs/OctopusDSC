9d89c89f3af1b2288682e6f38afbf6dd7969c668
alfhenrik <henrik.andersson@octopus.com>
2017-01-06 12:14:16 +1000

    Ooops, I can't PowerShell

--- OctopusDSC/DSCResources/cOctopusServer/cOctopusServer.psm1
+++ OctopusDSC/DSCResources/cOctopusServer/cOctopusServer.psm1
@@ -519,7 +519,7 @@ function Stop-OctopusDeployService($name)
     '--instance', $name
   )
   Invoke-OctopusServerCommand $args
-  Start-Sleep -Minutes 5
+  Start-Sleep -Seconds 300
 }
 
 function Get-ServiceName
